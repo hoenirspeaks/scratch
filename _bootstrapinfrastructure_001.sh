7d7fa467bb3ea0dc85b5f1a4ee0c4957da5af2d1
hoenirspeaks <95497550+hoenirspeaks@users.noreply.github.com>
2022-02-05 14:43:22 -0500

    Update _bootstrapinfrastructure_001.sh

--- _bootstrapinfrastructure_001.sh
+++ _bootstrapinfrastructure_001.sh
@@ -1,2 +1,4 @@
 #
-wget -q --no-cache -O - "https://raw.githubusercontent.com/hoenirspeaks/scratch/main/_bootstrapinfrastructure_002.sh"
+# wget -q --no-cache -O - "https://raw.githubusercontent.com/hoenirspeaks/scratch/main/_bootstrapinfrastructure_002.sh"
+apt-get -y update && apt-get -y upgrade
+
