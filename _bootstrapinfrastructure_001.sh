b77555c5d60e756c09cdec074dca547ec517f31c
hoenirspeaks <95497550+hoenirspeaks@users.noreply.github.com>
2022-02-05 14:21:12 -0500

    Update _bootstrapinfrastructure_001.sh

--- _bootstrapinfrastructure_001.sh
+++ _bootstrapinfrastructure_001.sh
@@ -1,5 +1,2 @@
 #
-cd ~
-wget https://raw.githubusercontent.com/hoenirspeaks/scratch/main/_bootstrapinfrastructure_002.sh
-[ -f ~/_bootstrapinfrastructure_002.sh -a -s _bootstrapinfrastructure_002.sh ] && \
-~/_bootstrapinfrastructure_002.sh
+echo "Hello, world!"
